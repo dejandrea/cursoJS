82e2bfa014d6a5612d433982fd1486b7df5fe957
Andréa J S França <dejandrea@gmail.com>
2022-03-27 10:18:15 -0300

    first commit

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50;
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel > 60) {
+    console.log("Limite de velocidade Atingido! Multado!")
+}
+
+console.log("dirija com cuidade e use sempre o sinto de segurança!")
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = "Brasil";
+console.log(`Vivendo em ${pais}`);
+
+if (pais == "Brasil") {
+    console.log("você é brasileiro")
+} else {
+    console.log("Você é estrangeiro")
+}
